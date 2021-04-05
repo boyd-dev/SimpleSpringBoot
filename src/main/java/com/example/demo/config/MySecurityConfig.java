ce5e9ff0b39766a4351b4862e9fc5f6389643bfb
kate-foo <kate-foo@company.com>
2021-04-05 21:23:04 +0900

    .

--- src/main/java/com/example/demo/config/MySecurityConfig.java
+++ src/main/java/com/example/demo/config/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Configuration
 @EnableWebSecurity(debug=false)
@@ -26,26 +30,30 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 		     .and()
 		     .csrf().disable() // 활성화하면 로그아웃시 POST 메소드로 해야 한다!
 		 .oauth2Login()  // http://localhost:8080/login 이면 디폴트 로그인 화면 표시
-		     .loginPage("/oauth2Login")		     
-		     //.successHandler(null)
-		     .redirectionEndpoint()
-		         .baseUri("/oauth2/callback/*") // 디폴트는 login/oauth2/code/*
-		         .and()
-		     .defaultSuccessUrl("/main.do", true)
-		     //.failureUrl("/main.do")		     
-		     .and()		      
-		  .logout()
-		     //.clearAuthentication(true)
-		     .deleteCookies("JSESSIONID")
-		     //.invalidateHttpSession(true) 
-		     .and()
+		    .loginPage("/oauth2Login")		     
+		       //.successHandler(null)
+		       .redirectionEndpoint()
+		          .baseUri("/oauth2/callback/*") // 디폴트는 login/oauth2/code/*
+		          .and()
+		       .userInfoEndpoint().userService(customOAuth2UserService())
+		          .and()
+		       .defaultSuccessUrl("/main.do", true)
+		       //.failureUrl("/main.do")		     
+		       .and()		      
+		    .logout()
+		       //.clearAuthentication(true)
+		       .deleteCookies("JSESSIONID")
+		       //.invalidateHttpSession(true) 
+		       .and()
+		    
+		    
+		  
 		  // 이렇게 하면 로그인 페이지가 나오지 않고 401이 리턴된다.   
 		  //.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 		     ;		     
 	}
 	
 	
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 				
@@ -54,6 +62,11 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 		    .withUser("user")
 		    .password(encoder.encode("1234"))
 		    .roles("USER");
+	}
+	
+	@Bean
+	public OAuth2UserService<OAuth2UserRequest, OAuth2User> customOAuth2UserService() {		
+		return new CustomOAuth2UserService();
 	}
 
 	
--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,46 +1,43 @@
 package com.example.demo.controller;
 
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 
 
 @Controller
 public class HomeController {
 	
-
+	private static final Logger logger = LogManager.getLogger(HomeController.class);
+	
 	@Autowired
 	private Environment env;
-
-
-	@Autowired
-	private OAuth2AuthorizedClientService authorizedClientService;
 	
 	@RequestMapping(value="/main.do")
-    public String mainPage(HttpServletRequest req, HttpServletResponse resp, OAuth2AuthenticationToken auth) {
+    public String mainPage(HttpServletRequest req, HttpServletResponse resp, OAuth2AuthenticationToken auth, Principal principal) {
 		
-		OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(auth.getAuthorizedClientRegistrationId(), auth.getName());
 		
-		System.out.println(client.getAccessToken().getTokenValue());
-		
-		// 리소스 서버에서 사용자 정보 가져오기
-		System.out.println(this.getUserInfo(client));
-		
+		if (logger.isDebugEnabled()) {
+			//logger.debug(result);
+			
+			// 액세스 토큰으로 사용자 정보를 수동으로 가져올 필요가 없을 것 같다. 시큐리티에서 사용자 프로파일 정보를 처리하여 이미 Principal 객체로 전달해주고 있다! 
+			
+			if (principal != null) {
+				logger.debug(principal.getName());
+			}
+		}
+			
 		
 		return "main.html";
     }
@@ -49,35 +46,10 @@ public class HomeController {
 	@RequestMapping(value="/oauth2Login", method = RequestMethod.GET)
     public String loginPage() {
 		
-		System.out.println(env.getProperty("myprops.name.age"));		
+		System.out.println(env.getProperty("myprops.name.age"));	
 		return "index.html";
     }
 	
-	private String getUserInfo(OAuth2AuthorizedClient client) {
 		
-		String resourceServerUri = client.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
-		String accessToken = client.getAccessToken().getTokenValue();		
-
-		String response = "";
-				
-		if (resourceServerUri != null && !"".equals(resourceServerUri) && accessToken != null && !"".equals(accessToken)) {
-			
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-			headers.set("Authorization", "Bearer " + accessToken);
-
-			MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-			
-			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-			RestTemplate restTemplate = new RestTemplate();
-			response = restTemplate.postForObject(resourceServerUri, request, String.class);
-
-		}
-		//TODO 예외발생시 처리
-		//
-		
-		return response;
-	}
-	
 
 }
